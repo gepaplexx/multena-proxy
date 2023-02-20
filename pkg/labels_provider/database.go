499f4219c38a95f7385063f6e95ecdc2dcb66b4c
apatecwagnerc <125589500+apatecwagnerc@users.noreply.github.com>
2023-02-20 10:13:52 +0100

    Added MySQL support (#12)
    * Added MySQL support

--- pkg/labels_provider/database.go
+++ pkg/labels_provider/database.go
@@ -3,23 +3,46 @@ package labels_provider
 import (
 	"database/sql"
 	"github.com/gepaplexx/multena-proxy/pkg/utils"
+	"os"
+	"strings"
 )
+
+//const stmt = "SELECT sla.service_instance AS bla " +
+//	"FROM person p " +
+//	"JOIN service_level_agreement_coverage_person slap ON slap.person = p.id " +
+//	"JOIN service_level_agreement sla ON slap.serviceLevelAgreement = sla.id " +
+//	"WHERE p.primary_email = ? " +
+//	"AND p.disabled = 0 " +
+//	"UNION " +
+//	"SELECT sla.service_instance AS bla " +
+//	"FROM person p " +
+//	"JOIN service_level_agreement_coverage_organization slao ON slao.organization = p.organization " +
+//	"JOIN service_level_agreement sla ON slao.serviceLevelAgreement = sla.id " +
+//	"WHERE p.primary_email = ? " +
+//	"AND p.disabled = 0"
 
 func GetLabelsFromDB(email string) []string {
 	db := utils.DB
-	res, err := db.Query("SELECT * FROM user WHERE email = ?", email)
+	stmt := os.Getenv("LABEL_DB_QUERY")
+	n := strings.Count(stmt, "?")
+
+	var params []any
+	for i := 0; i < n; i++ {
+		params = append(params, email)
+	}
+
+	res, err := db.Query(stmt, params...)
 	defer func(res *sql.Rows) {
 		err := res.Close()
 		utils.LogIfPanic("Error closing result", err)
 	}(res)
 	utils.LogIfPanic("Error querying database", err)
-	labels := []string{}
-	if res.Next() {
-		err := res.Scan(&labels)
+	var labels []string
+	for res.Next() {
+		var label string
+		err = res.Scan(&label)
+		labels = append(labels, label)
 		utils.LogIfPanic("Error scanning result", err)
-	} else {
-		utils.LogIfError("Error no rows", err)
 	}
 	return labels
-
 }
--- pkg/rewrite/urlrewriter.go
+++ pkg/rewrite/urlrewriter.go
@@ -7,10 +7,20 @@ import (
 
 func UrlRewriter(URL string, labels []string, tenantLabel string) string {
 	// write tests for this function and make it more robust
-	quIn := strings.Index(URL, "?") + 1
+	quIn := strings.Index(URL, "?")
+	if quIn == -1 {
+		URL += "?"
+		quIn = len(URL)
+	} else {
+		quIn += 1
+	}
+
 	labelsEnforcer := ""
 	for _, label := range labels {
 		labelsEnforcer += fmt.Sprintf("%s=%s&", tenantLabel, label)
+	}
+	if labelsEnforcer != "" && URL[quIn:] == "" {
+		labelsEnforcer = labelsEnforcer[:len(labelsEnforcer)-1]
 	}
 	url := URL[:quIn] + labelsEnforcer + URL[quIn:]
 	return url
