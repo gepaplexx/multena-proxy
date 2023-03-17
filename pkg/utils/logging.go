b124bb4975596167479cd850bcb6e6e9e358c946
lucostus <constantin.winkler@gepardec.com>
2023-03-17 14:32:34 +0100

    GPX-580: added comments
    Signed-off-by: lucostus <constantin.winkler@gepardec.com>

--- pkg/utils/logging.go
+++ pkg/utils/logging.go
@@ -11,6 +11,9 @@ var (
 	Logger *zap.Logger
 )
 
+// InitLogging initializes the logger
+// The log level is set in the config file
+// The log level can be set to debug, info, warn, error, dpanic, panic, or fatal
 func InitLogging() {
 	rawJSON := []byte(`{
 		"level": "` + strings.ToLower(C.Proxy.LogLevel) + `",
@@ -34,12 +37,14 @@ func InitLogging() {
 	Logger.Debug("Go Version", zap.String("version", runtime.Version()))
 }
 
+// LogIfPanic logs an error if it is not nil and panics
 func LogIfPanic(msg string, err error) {
 	if err != nil {
 		Logger.Panic(msg, zap.String("error", err.Error()))
 	}
 }
 
+// LogIfError logs an error if it is not nil and continues execution
 func LogIfError(msg string, err error) {
 	if err != nil {
 		Logger.Error(msg, zap.String("error", err.Error()))
