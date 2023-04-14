683e68f2e226d8b95e1d0bcecd4398c31ae5d43c
lucostus <constantin.winkler@gepardec.com>
2023-04-14 18:48:10 +0200

    GPX-580: WIP

--- labeler.go
+++ labeler.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"go.uber.org/zap"
 	"golang.org/x/net/context"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -68,4 +70,76 @@ func GetLabelsFromProject(username string) []string {
 	}
 	Logger.Debug("Finished Searching namespaces")
 	return namespaces
+}
+
+func enforceNamespaces(query string, namespaces []string) (string, error) {
+	if len(namespaces) == 0 {
+		return "", errors.New("namespaces slice cannot be empty")
+	}
+
+	namespaceRegex := regexp.MustCompile(`kubernetes_namespace_name\s*=\s*"?([^"{}\s,|]+)"?`)
+	namespaceListRegex := regexp.MustCompile(`kubernetes_namespace_name\s*=\s*~\s*"?([^"{}\s,]+)"?`)
+	selectorRegex := regexp.MustCompile(`\{([^\{\}]+)\}`)
+	goTemplateRegex := regexp.MustCompile(`{{[^}]*}}`)
+
+	enforcedNamespaceList := strings.Join(namespaces, "|")
+
+	// Check if kubernetes_namespace_name is already in the query and if it contains disallowed namespaces
+	if namespaceRegex.MatchString(query) || namespaceListRegex.MatchString(query) {
+		namespacesFound := make(map[string]bool)
+		for _, match := range namespaceRegex.FindAllStringSubmatch(query, -1) {
+			namespacesFound[match[1]] = true
+		}
+		for _, match := range namespaceListRegex.FindAllStringSubmatch(query, -1) {
+			for _, ns := range strings.Split(match[1], "|") {
+				namespacesFound[ns] = true
+			}
+		}
+
+		for ns := range namespacesFound {
+			if !contains(namespaces, ns) {
+				return "", errors.New("query contains disallowed namespaces")
+			}
+		}
+
+		return query, nil // Return the original query if it contains only allowed namespaces
+	}
+
+	// Split query into parts
+	parts := goTemplateRegex.Split(query, -1)
+	placeholders := goTemplateRegex.FindAllString(query, -1)
+
+	// Add kubernetes_namespace_name to the query if it's not present
+	for i, part := range parts {
+		if selectorRegex.MatchString(part) && !strings.Contains(part, "kubernetes_namespace_name") {
+			replacer := func(match string) string {
+				if len(namespaces) == 1 {
+					return fmt.Sprintf("{%s, kubernetes_namespace_name=%q}", match[1:len(match)-1], namespaces[0])
+				}
+				return fmt.Sprintf("{%s, kubernetes_namespace_name=~%q}", match[1:len(match)-1], enforcedNamespaceList)
+			}
+
+			parts[i] = selectorRegex.ReplaceAllStringFunc(part, replacer)
+		}
+	}
+
+	// Rejoin the parts and placeholders
+	var result strings.Builder
+	for i, part := range parts {
+		result.WriteString(part)
+		if i < len(placeholders) {
+			result.WriteString(placeholders[i])
+		}
+	}
+
+	return result.String(), nil
+}
+
+func contains(slice []string, item string) bool {
+	for _, i := range slice {
+		if i == item {
+			return true
+		}
+	}
+	return false
 }
---
+++ labeler_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+)
+
+type enforceTest struct {
+	query      string
+	namespaces []string
+	expected   string
+	error      error
+}
+
+var enforceTests = []enforceTest{
+	{`sum(rate({level="error"} [$__interval]))`, []string{"tenant1", "openshift-logging"}, `sum(rate({level="error", kubernetes_namespace_name=~"tenant1|openshift-logging"} [$__interval]))`, nil},
+	{`sum(rate({level="error", kubernetes_host="worker21"} [$__interval]))`, []string{"tenant1", "openshift-logging"}, `sum(rate({level="error", kubernetes_host="worker21", kubernetes_namespace_name=~"tenant1|openshift-logging"} [$__interval]))`, nil},
+	{`sum(rate({level="error", kubernetes_host="worker21", kubernetes_namespace_name="gepaplexx-cicd-tools"} [$__interval]))`, []string{"tenant1", "gepaplexx-cicd-tools"}, `sum(rate({level="error", kubernetes_host="worker21", kubernetes_namespace_name="gepaplexx-cicd-tools"} [$__interval]))`, nil},
+	{`sum(rate({level="error", kubernetes_host="worker21", kubernetes_namespace_name="gepaplexx-cicd-tools"} [$__interval]))`, []string{"tenant1", "tenant2"}, ``, errors.New("query contains disallowed namespaces")},
+	{`sum(rate({level="error", kubernetes_host="worker21", kubernetes_namespace_name=~"gepaplexx-cicd-tools|tenant1"} [$__interval]))`, []string{"tenant1", "gepaplexx-cicd-tools", "tenant3", "tenant_blubb"}, `sum(rate({level="error", kubernetes_host="worker21", kubernetes_namespace_name=~"gepaplexx-cicd-tools|tenant1"} [$__interval]))`, nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", []string{"multena"}, "sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", []string{"multena", "tenant2"}, "sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", nil},
+	{"sum by(level) (rate({level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", []string{"multena", "tenant2", "tenant3"}, "sum by(level) (rate({level=~\"error|default\", kubernetes_namespace_name=~\"multena|tenant2|tenant3\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", []string{"tenant2", "tenant3"}, "", errors.New("query contains disallowed namespaces")},
+	{"sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", []string{"multena", "grafana", "tenant3"}, "sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} |= `message` | json | line_format `{{.message}}` |= `message` | json | unpack | line_format `{{.message}}` [$__interval]))", []string{"multena"}, "", errors.New("query contains disallowed namespaces")},
+	{`{kubernetes_pod_name=~"admission-control-5df64f7bbf-66vls"}`, []string{"tenant1", "openshift-logging"}, `{kubernetes_pod_name=~"admission-control-5df64f7bbf-66vls", kubernetes_namespace_name=~"tenant1|openshift-logging"}`, nil},
+	{`{kubernetes_pod_name=~"admission-control-5df64f7bbf-66vls"}`, []string{"tenant1"}, `{kubernetes_pod_name=~"admission-control-5df64f7bbf-66vls", kubernetes_namespace_name="tenant1"}`, nil},
+	{`{kubernetes_pod_ip="10.128.4.133"} |= "proxy" | json`, []string{"tenant1", "openshift-logging"}, `{kubernetes_pod_ip="10.128.4.133", kubernetes_namespace_name=~"tenant1|openshift-logging"} |= "proxy" | json`, nil},
+	{`{kubernetes_pod_ip="10.128.4.133"} |= "proxy" | json`, []string{"tenant1", "openshift-logging", "kube-system", "default"}, `{kubernetes_pod_ip="10.128.4.133", kubernetes_namespace_name=~"tenant1|openshift-logging|kube-system|default"} |= "proxy" | json`, nil},
+	{`{kubernetes_pod_ip="10.128.4.133"} |= "proxy" | json`, []string{"openshift-logging"}, `{kubernetes_pod_ip="10.128.4.133", kubernetes_namespace_name="openshift-logging"} |= "proxy" | json`, nil},
+	{`sum(rate({kubernetes_namespace_name=~"cert-manager|erhard-pg"} | json | line_format "{{.status_code}}" [$__interval])) > 2`, []string{"cert-manager|erhard-pg"}, `sum(rate({kubernetes_namespace_name=~"cert-manager|erhard-pg"} | json | line_format "{{.status_code}}" [$__interval])) > 2`, nil},
+	{`sum(rate({kubernetes_namespace_name=~"cert-manager|erhard-pg"} | json | line_format "{{.status_code}}" [$__interval])) > 2`, []string{"cert-manager"}, `sum(rate({kubernetes_namespace_name="cert-manager"} | json | line_format "{{.status_code}}" [$__interval])) > 2`, nil},
+	{`sum(rate({kubernetes_namespace_name=~"cert-manager|erhard-pg|default"} | json | line_format "{{.status_code}}" [$__interval])) > 2`, []string{"cert-manager|erhard-pg"}, ``, errors.New("query contains disallowed namespaces")},
+	{`sum(rate({kubernetes_namespace_name=~"cert-manager|erhard-pg"} | json | line_format "{{.status_code}}" [$__interval])) > 2`, []string{"tenant1"}, "", errors.New("query contains disallowed namespaces")},
+	{`sum(rate({kubernetes_namespace_name=~"cert-manager|erhard-pg"} | json | line_format "{{.status_code}}" [$__interval])) > 2`, []string{"different_namespace"}, "", errors.New("query contains disallowed namespaces")},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) - sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"multena"}, "sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) - sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) + sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"multena", "grafana"}, "sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) + sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) - sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"grafana"}, "", errors.New("query contains disallowed namespaces")},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) > sum by(level) (rate({level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"multena"}, "sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) > sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) - sum by(level) (rate({level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"multena", "grafana"}, "sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) - sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) == sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"multena", "grafana"}, "sum by(level) (rate({kubernetes_namespace_name=\"multena|grafana\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) == sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) - sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"multena", "grafana", "openshift-logging"}, "sum by(level) (rate({kubernetes_namespace_name=\"multena|grafana\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) - sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", nil},
+	{"sum by(level) (rate({kubernetes_namespace_name=~\"multena|grafana\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval])) != sum by(level) (rate({kubernetes_namespace_name=\"multena\", level=~\"error|default\"} | json | line_format `{{.message}}` | json | line_format `{{.message}}` | __error__ = `` [$__interval]))", []string{"multena", "openshift-logging"}, "", errors.New("query contains disallowed namespaces")},
+}
+
+func TestEnforcer(t *testing.T) {
+	outf, _ := os.Create("output")
+	defer outf.Close()
+	expf, _ := os.Create("expected")
+	defer expf.Close()
+	for i, test := range enforceTests {
+		if output, err := enforceNamespaces(test.query, test.namespaces); (err == test.error) && (output != test.expected) {
+			if err != test.error {
+				_, _ = outf.WriteString(fmt.Sprint(i) + fmt.Sprint(err) + "\n")
+				_, _ = expf.WriteString(fmt.Sprint(i) + fmt.Sprint(test.error) + "\n")
+				t.Logf("Error %q not equal to expected %q", err, test.error)
+			} else {
+				_, _ = outf.WriteString(fmt.Sprint(i) + output + "\n")
+				_, _ = expf.WriteString(fmt.Sprint(i) + test.expected + "\n")
+				t.Errorf("Output %q not equal to expected %q", output, test.expected)
+			}
+
+		}
+	}
+}
