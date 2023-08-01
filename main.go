d9b77e0b64d3b469537b592f8f37b4c02e0f3797
Constantin <constantin.winkler@gepardec.com>
2023-08-01 18:13:18 +0200

    Refactor main.go for optimization and cleanup
    Removed unused package imports and handlers including promhttp handler, pprof Index handler, and HealthCheckHandler because they were not needed anymore. Also re-arranged the http server initialization process.

--- main.go
+++ main.go
@@ -2,20 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
 	"github.com/slok/go-http-metrics/middleware"
 	"github.com/slok/go-http-metrics/middleware/std"
 	"go.uber.org/zap"
 	"net/http"
-	"net/http/pprof"
 )
 
 // main is the entry point. It initializes necessary components, sets up HTTP routes, and starts the HTTP server.
 func main() {
 	defer func(Logger *zap.Logger) {
 		err := Logger.Sync()
-
 		if err != nil {
 			fmt.Printf("{\"level\":\"error\",\"error\":\"%s/\"}", err)
 			return
@@ -24,31 +21,26 @@ func main() {
 
 	Logger.Info("Starting Proxy")
 
+	e, i, err := application()
+
+	go func() {
+		if err := http.ListenAndServe(fmt.Sprintf("%s:%d", Cfg.Web.Host, Cfg.Web.MetricsPort), i); err != nil {
+			Logger.Panic("Error while serving metrics", zap.Error(err))
+		}
+	}()
+
+	if err != nil {
+		Logger.Panic("Error while initializing application", zap.Error(err))
+	}
+
 	mdlw := middleware.New(middleware.Config{
 		Recorder: metrics.NewRecorder(metrics.Config{}),
 		Service:  "multena",
 	})
-	mux := http.NewServeMux()
-	mux.Handle("/metrics", promhttp.Handler())
-	mux.Handle("/healthz", http.HandlerFunc(HealthCheckHandler))
-	mux.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
-
-	go func() {
-		if err := http.ListenAndServe(fmt.Sprintf("%s:%d", Cfg.Web.Host, Cfg.Web.MetricsPort), mux); err != nil {
-			Logger.Panic("Error while serving metrics", zap.Error(err))
-		}
-	}()
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", Cfg.Web.Host, Cfg.Web.ProxyPort),
-		std.Handler("/", mdlw, application()))
+	err = http.ListenAndServe(fmt.Sprintf("%s:%d", Cfg.Web.Host, Cfg.Web.ProxyPort),
+		std.Handler("/", mdlw, e))
 
 	if err != nil {
 		Logger.Panic("Error while serving", zap.Error(err))
 	}
-}
-
-// HealthCheckHandler is an HTTP handler that always returns an HTTP status of 200 and a response body of "Ok".
-// It's commonly used for health checks.
-func HealthCheckHandler(responseWriter http.ResponseWriter, _ *http.Request) {
-	responseWriter.WriteHeader(http.StatusOK)
-	_, _ = fmt.Fprintf(responseWriter, "%s", "Ok")
 }
