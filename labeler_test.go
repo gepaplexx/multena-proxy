f9f35ec9bdd180d5236a991b8401fd03867b9d46
Constantin <constantin.winkler@gepardec.com>
2023-08-01 18:10:46 +0200

    GPX-698: adjust test

--- labeler_test.go
+++ labeler_test.go
@@ -68,7 +68,7 @@ func TestGetLabelsCM(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			labels := GetLabelsCM(tc.username, tc.groups)
+			labels := GetLabelsCM(KeycloakToken{PreferredUsername: tc.username, Groups: tc.groups})
 			assert.Equal(t, tc.expected, labels)
 		})
 	}
