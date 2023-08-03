562773d3023d2236bbc5c078551695989a4c7343
Constantin <constantin.winkler@gepardec.com>
2023-08-03 15:16:57 +0200

    GPX-698: Add Config struct to init.go.

--- init.go
+++ init.go
@@ -32,6 +32,63 @@ var (
 	atomicLevel         zap.AtomicLevel
 )
 
+type Config struct {
+	Log struct {
+		Level     string `mapstructure:"level"`
+		LogTokens bool   `mapstructure:"log_tokens"`
+	} `mapstructure:"log"`
+
+	TenantProvider string `mapstructure:"tenant_provider"`
+
+	Web struct {
+		ProxyPort          int    `mapstructure:"proxy_port"`
+		MetricsPort        int    `mapstructure:"metrics_port"`
+		Host               string `mapstructure:"host"`
+		InsecureSkipVerify bool   `mapstructure:"insecure_skip_verify"`
+		TrustedRootCaPath  string `mapstructure:"trusted_root_ca_path"`
+		JwksCertURL        string `mapstructure:"jwks_cert_url"`
+	} `mapstructure:"web"`
+
+	Admin struct {
+		Bypass bool   `mapstructure:"bypass"`
+		Group  string `mapstructure:"group"`
+	} `mapstructure:"admin"`
+
+	Dev struct {
+		Enabled             bool   `mapstructure:"enabled"`
+		Username            string `mapstructure:"username"`
+		ServiceAccountToken string `mapstructure:"service_account_token"`
+	} `mapstructure:"dev"`
+
+	Db struct {
+		Enabled      bool   `mapstructure:"enabled"`
+		User         string `mapstructure:"user"`
+		PasswordPath string `mapstructure:"password_path"`
+		Host         string `mapstructure:"host"`
+		Port         int    `mapstructure:"port"`
+		DbName       string `mapstructure:"dbName"`
+		Query        string `mapstructure:"query"`
+	} `mapstructure:"db"`
+
+	Thanos struct {
+		URL          string `mapstructure:"url"`
+		TenantLabel  string `mapstructure:"tenant_label"`
+		UseMutualTLS bool   `mapstructure:"use_mutual_tls"`
+		Cert         string `mapstructure:"cert"`
+		Key          string `mapstructure:"key"`
+	} `mapstructure:"thanos"`
+	Loki struct {
+		URL          string `mapstructure:"url"`
+		TenantLabel  string `mapstructure:"tenant_label"`
+		UseMutualTLS bool   `mapstructure:"use_mutual_tls"`
+		Cert         string `mapstructure:"cert"`
+		Key          string `mapstructure:"key"`
+	} `mapstructure:"loki"`
+
+	Users  map[string][]string `mapstructure:"users"`
+	Groups map[string][]string `mapstructure:"groups"`
+}
+
 // init carries out the main initialization routine for the Proxy. It logs the commit information,
 // configures the HTTP client to ignore self-signed certificates, reads the service account token,
 // initializes JWKS if not in development mode, and establishes a database connection if enabled in the config.
