0577bd2d173427bc714adebb32656d4290332c20
Constantin <constantin.winkler@gepardec.com>
2023-08-03 15:21:41 +0200

    GPX-698: Refactor and enhance the routing and security structure
    This commit modifies the 'routes.go' file to substantially improve how routes are assigned and configured by creating separate handlers for Loki and Thanos API endpoints. Alongside this, the Datasource struct has been introduced to encapsulate all necessary information per data source allowing us to handle route via EnforeceFunc. The changes also consist of the implementation of 'UseMutualTLS' in the struct to increase security measures when communicating with upstream servers.

--- routes.go
+++ routes.go
@@ -1,32 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 	"net/http/pprof"
 	"net/url"
 )
 
 type Route struct {
-	Url        string
-	Datasource string
-	MatchWord  string
+	Url       string
+	MatchWord string
 }
 
-type kkToken struct{}
+type EnforceFunc func(string, map[string]bool) (string, error)
+
+type Datasource struct {
+	UpstreamURL  *url.URL
+	EnforceFunc  EnforceFunc
+	UseMutualTLS bool
+}
+
+type contextKey string
+
+const DatasourceKey contextKey = "datasource"
+const KeycloakCtxToken contextKey = "keycloakToken"
 
 // application initializes the application's HTTP router
 func application() (*mux.Router, *mux.Router, error) {
-
-	i := mux.NewRouter()
-	i.HandleFunc("/health", HealthCheckHandler)
-	i.HandleFunc("/debug/pprof/", pprof.Index)
-	i.Handle("/metrics", promhttp.Handler())
-
-	e := mux.NewRouter()
-	e.Use(loggingMiddleware)
-	e.Use(authMiddleware)
 
 	lokiUrl, err := url.Parse(Cfg.Loki.URL)
 	if err != nil {
@@ -38,58 +41,58 @@ func application() (*mux.Router, *mux.Router, error) {
 		return nil, nil, fmt.Errorf("error parsing Thanos URL: %v", err)
 	}
 
-	routes, err := setupRoutes()
-	if err != nil {
-		return nil, nil, fmt.Errorf("error setting up routes: %v", err)
+	i := mux.NewRouter()
+	i.HandleFunc("/health", HealthCheckHandler)
+	i.HandleFunc("/debug/pprof/", pprof.Index)
+	i.Handle("/metrics", promhttp.Handler())
+
+	routes := []Route{
+		{Url: "/api/v1/query", MatchWord: "query"},
+		{Url: "/api/v1/query_range", MatchWord: "query"},
+		{Url: "/api/v1/label/{name}/values", MatchWord: "query"},
+		{Url: "/api/v1/series", MatchWord: "match[]"},
+		{Url: "/api/v1/tail", MatchWord: "query"},
+		{Url: "/api/v1/index/stats", MatchWord: "query"},
+		{Url: "/api/v1/format_query", MatchWord: "query"},
+		{Url: "/api/v1/labels", MatchWord: "match[]"},
+		{Url: "/api/v1/label/{label}/values", MatchWord: "match[]"},
+		{Url: "/api/v1/query_exemplars", MatchWord: "query"},
+		{Url: "/api/v1/status/buildinfo", MatchWord: "query"},
 	}
 
+	e := mux.NewRouter()
+
+	lokiRouter := e.PathPrefix("/loki").Subrouter()
+	thanosRouter := e.PathPrefix("").Subrouter()
+
+	lokiRouter.Use(setDatasource(Datasource{
+		UpstreamURL:  lokiUrl,
+		EnforceFunc:  logqlEnforcer,
+		UseMutualTLS: Cfg.Loki.UseMutualTLS,
+	}))
+	thanosRouter.Use(setDatasource(Datasource{
+		UpstreamURL:  thanosUrl,
+		EnforceFunc:  promqlEnforcer,
+		UseMutualTLS: Cfg.Thanos.UseMutualTLS,
+	}))
+
+	e.Use(loggingMiddleware)
+	e.Use(authMiddleware)
+
 	for _, route := range routes {
-		if err := handleRoute(e, route, thanosUrl, lokiUrl); err != nil {
-			return nil, nil, fmt.Errorf("error handling route: %v", err)
-		}
+		handleRoute(lokiRouter, route)
+		handleRoute(thanosRouter, route)
 	}
 
 	e.SkipClean(true)
 	return e, i, nil
 }
 
-// getRouteConfiguration returns the corresponding URL and function for a route
-func getRouteConfiguration(route Route, thanosUrl *url.URL, lokiUrl *url.URL) (*url.URL, func(string, map[string]bool) (string, error), error) {
-	var datasourceURL *url.URL
-	var enforceFunc func(string, map[string]bool) (string, error)
-
-	if route.Datasource == "thanos" {
-		datasourceURL = thanosUrl
-		enforceFunc = promqlEnforcer
-	} else if route.Datasource == "loki" {
-		datasourceURL = lokiUrl
-		enforceFunc = logqlEnforcer
-	} else {
-		return nil, nil, fmt.Errorf("unsupported datasource: %s", route.Datasource)
+func setDatasource(ds Datasource) mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), DatasourceKey, ds)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
 	}
-
-	return datasourceURL, enforceFunc, nil
-}
-
-// setupRoutes sets up the application's routes
-func setupRoutes() ([]Route, error) {
-	routes := []Route{
-		//loki
-		{Url: "/loki/api/v1/query", Datasource: "loki", MatchWord: "query"},
-		{Url: "/loki/api/v1/query_range", Datasource: "loki", MatchWord: "query"},
-		{Url: "/loki/api/v1/label/{name}/values", Datasource: "loki", MatchWord: "query"},
-		{Url: "/loki/api/v1/series", Datasource: "loki", MatchWord: "match[]"},
-		{Url: "/loki/api/v1/tail", Datasource: "loki", MatchWord: "query"},
-		{Url: "/loki/api/v1/index/stats", Datasource: "loki", MatchWord: "query"},
-		// Thanos
-		{Url: "/api/v1/query", Datasource: "thanos", MatchWord: "query"},
-		{Url: "/api/v1/query_range", Datasource: "thanos", MatchWord: "query"},
-		{Url: "/api/v1/format_query", Datasource: "thanos", MatchWord: "query"},
-		{Url: "/api/v1/series", Datasource: "thanos", MatchWord: "match[]"},
-		{Url: "/api/v1/labels", Datasource: "thanos", MatchWord: "match[]"},
-		{Url: "/api/v1/label/{label}/values", Datasource: "thanos", MatchWord: "match[]"},
-		{Url: "/api/v1/query_exemplars", Datasource: "thanos", MatchWord: "query"},
-		{Url: "/api/v1/status/buildinfo", Datasource: "thanos", MatchWord: "query"},
-	}
-	return routes, nil
 }
