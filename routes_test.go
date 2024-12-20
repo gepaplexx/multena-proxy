5dcfa8dd1be9dc580993a61bc4949cffed638dac
lucostus <constantin.winkler@gepardec.com>
2024-12-20 11:34:13 +0100

    Support local jwks cert definition

--- routes_test.go
+++ routes_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -46,4 +47,72 @@ func TestSetActorHeaderPromQL(t *testing.T) {
 	err := setActorHeaderPromQL(req, token, app)
 	assert.NoError(t, err)
 	assert.Equal(t, "dXNlcnVzZXJAZXhhbXBsZS5jb20=", req.Header.Get("X-Actor"))
+}
+
+func TestWithHealthz(t *testing.T) {
+	app := &App{
+		Cfg: &Config{
+			Alert: AlertConfig{
+				Enabled: false,
+			},
+		},
+	}
+
+	app = app.WithHealthz()
+
+	ts := httptest.NewServer(app.i)
+	defer ts.Close()
+
+	t.Run("Healthz OK", func(t *testing.T) {
+		app.healthy = true
+		resp, err := http.Get(ts.URL + "/healthz")
+		if err != nil {
+			t.Fatalf("Failed to send GET request: %v", err)
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				t.Fatalf("Failed to close response body: %v", err)
+			}
+		}(resp.Body)
+
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status %d, got %d", http.StatusOK, resp.StatusCode)
+		}
+	})
+
+	t.Run("Healthz Not OK", func(t *testing.T) {
+		app.healthy = false
+		resp, err := http.Get(ts.URL + "/healthz")
+		if err != nil {
+			t.Fatalf("Failed to send GET request: %v", err)
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				t.Fatalf("Failed to close response body: %v", err)
+			}
+		}(resp.Body)
+
+		if resp.StatusCode != http.StatusInternalServerError {
+			t.Errorf("Expected status %d, got %d", http.StatusInternalServerError, resp.StatusCode)
+		}
+	})
+
+	t.Run("Metrics endpoint", func(t *testing.T) {
+		resp, err := http.Get(ts.URL + "/metrics")
+		if err != nil {
+			t.Fatalf("Failed to send GET request: %v", err)
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				t.Fatalf("Failed to close response body: %v", err)
+			}
+		}(resp.Body)
+
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status %d, got %d", http.StatusOK, resp.StatusCode)
+		}
+	})
 }
